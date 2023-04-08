33e193d1e041187a1e66c28f3d25e813dbe20411
Kent Nathaniel <kent.nathaniel2@gmail.com>
2023-04-08 14:40:43 +0700

    Enable config cors

--- next.config.js
+++ next.config.js
@@ -2,8 +2,23 @@
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
-  images: {
-    domains: ["db.nteimmronqaznldgopsg.supabase.co"],
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/v1/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET, OPTIONS, PATCH, DELETE, POST, PUT" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
   },
 };
 
