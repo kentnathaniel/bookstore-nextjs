34a6a3268904a12501aa9bc3a7377e13d3b29671
Kent Nathaniel <kent.nathaniel2@gmail.com>
2023-04-08 14:47:26 +0700

    Update base url

--- src/modules/axios/index.js
+++ src/modules/axios/index.js
@@ -1,16 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Set up Axios instance
-const baseURL = 'http://localhost:3000/api';
+const baseURL = `${process.env.NEXT_PUBLIC_BASE_URL}`;
 const instance = axios.create({ baseURL });
 
 // Add interceptor to automatically add authorization header
 instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
 
-export { instance  };
+export { instance };
